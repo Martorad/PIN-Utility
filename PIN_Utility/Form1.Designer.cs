8c7f916413c0da4dfb32d5a4885d92cd46b45f82
Martin Kiryakov <m.kiryakov@student.fontys.nl>
2020-10-05 00:08:19 +0200

    Added file for STM32 Nucleo regarding interrupts, timer setup and GPIO setup.

--- PIN_Utility/Form1.Designer.cs
+++ PIN_Utility/Form1.Designer.cs
@@ -60,9 +60,10 @@
             // pbMain
             // 
             this.pbMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pbMain.Location = new System.Drawing.Point(25, 24);
+            this.pbMain.Location = new System.Drawing.Point(19, 20);
+            this.pbMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbMain.Name = "pbMain";
-            this.pbMain.Size = new System.Drawing.Size(917, 115);
+            this.pbMain.Size = new System.Drawing.Size(688, 93);
             this.pbMain.TabIndex = 0;
             this.pbMain.TabStop = false;
             // 
@@ -81,25 +82,28 @@
             this.pnlMain.Controls.Add(this.tbPath);
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(966, 353);
+            this.pnlMain.Size = new System.Drawing.Size(724, 293);
             this.pnlMain.TabIndex = 1;
             // 
             // lblOutput
             // 
             this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(12, 81);
+            this.lblOutput.Location = new System.Drawing.Point(9, 66);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(306, 17);
+            this.lblOutput.Size = new System.Drawing.Size(227, 13);
             this.lblOutput.TabIndex = 7;
             this.lblOutput.Text = "Please enter the desired path for the output file";
             this.lblOutput.Visible = false;
             // 
             // tbOutput
             // 
-            this.tbOutput.Location = new System.Drawing.Point(12, 101);
+            this.tbOutput.Location = new System.Drawing.Point(9, 82);
+            this.tbOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbOutput.Name = "tbOutput";
-            this.tbOutput.Size = new System.Drawing.Size(477, 22);
+            this.tbOutput.Size = new System.Drawing.Size(359, 20);
             this.tbOutput.TabIndex = 6;
             this.tbOutput.Visible = false;
             // 
@@ -108,9 +112,10 @@
             this.cbOutput.AutoSize = true;
             this.cbOutput.Checked = true;
             this.cbOutput.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbOutput.Location = new System.Drawing.Point(12, 58);
+            this.cbOutput.Location = new System.Drawing.Point(9, 47);
+            this.cbOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbOutput.Name = "cbOutput";
-            this.cbOutput.Size = new System.Drawing.Size(221, 21);
+            this.cbOutput.Size = new System.Drawing.Size(168, 17);
             this.cbOutput.TabIndex = 4;
             this.cbOutput.Text = "Save output file in same folder";
             this.cbOutput.UseVisualStyleBackColor = true;
@@ -118,9 +123,10 @@
             // 
             // btnDone
             // 
-            this.btnDone.Location = new System.Drawing.Point(12, 290);
+            this.btnDone.Location = new System.Drawing.Point(9, 236);
+            this.btnDone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDone.Name = "btnDone";
-            this.btnDone.Size = new System.Drawing.Size(477, 52);
+            this.btnDone.Size = new System.Drawing.Size(358, 42);
             this.btnDone.TabIndex = 5;
             this.btnDone.Text = "Done";
             this.btnDone.UseVisualStyleBackColor = true;
@@ -129,36 +135,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(379, 17);
+            this.label1.Size = new System.Drawing.Size(281, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Please paste the folder path (please use an absolute path)";
             // 
             // tbPath
             // 
-            this.tbPath.Location = new System.Drawing.Point(12, 29);
+            this.tbPath.Location = new System.Drawing.Point(9, 24);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(477, 22);
+            this.tbPath.Size = new System.Drawing.Size(359, 20);
             this.tbPath.TabIndex = 0;
             this.tbPath.Text = "C:\\Users\\marti\\Desktop\\PIN_images";
             // 
             // progressMain
             // 
-            this.progressMain.Location = new System.Drawing.Point(22, 236);
+            this.progressMain.Location = new System.Drawing.Point(16, 192);
+            this.progressMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressMain.MarqueeAnimationSpeed = 0;
             this.progressMain.Name = "progressMain";
-            this.progressMain.Size = new System.Drawing.Size(920, 27);
+            this.progressMain.Size = new System.Drawing.Size(690, 22);
             this.progressMain.TabIndex = 2;
             // 
             // tbMainInput
             // 
             this.tbMainInput.AcceptsReturn = true;
             this.tbMainInput.Font = new System.Drawing.Font("OcrB", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbMainInput.Location = new System.Drawing.Point(72, 143);
+            this.tbMainInput.Location = new System.Drawing.Point(54, 116);
+            this.tbMainInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbMainInput.MaxLength = 19;
             this.tbMainInput.Name = "tbMainInput";
-            this.tbMainInput.Size = new System.Drawing.Size(843, 87);
+            this.tbMainInput.Size = new System.Drawing.Size(633, 71);
             this.tbMainInput.TabIndex = 3;
             this.tbMainInput.TextChanged += new System.EventHandler(this.TbMainInput_TextChanged);
             this.tbMainInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TbMainInput_KeyDown);
@@ -167,9 +177,10 @@
             // 
             this.btnBack.Enabled = false;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBack.Location = new System.Drawing.Point(423, 270);
+            this.btnBack.Location = new System.Drawing.Point(317, 219);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(169, 74);
+            this.btnBack.Size = new System.Drawing.Size(127, 60);
             this.btnBack.TabIndex = 6;
             this.btnBack.Text = "<";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -181,9 +192,10 @@
             this.lblOverlay.BackColor = System.Drawing.Color.Transparent;
             this.lblOverlay.Font = new System.Drawing.Font("OcrB", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblOverlay.ForeColor = System.Drawing.Color.Red;
-            this.lblOverlay.Location = new System.Drawing.Point(65, 70);
+            this.lblOverlay.Location = new System.Drawing.Point(49, 57);
+            this.lblOverlay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOverlay.Name = "lblOverlay";
-            this.lblOverlay.Size = new System.Drawing.Size(0, 74);
+            this.lblOverlay.Size = new System.Drawing.Size(0, 58);
             this.lblOverlay.TabIndex = 7;
             // 
             // cbOverlay
@@ -191,9 +203,10 @@
             this.cbOverlay.AutoSize = true;
             this.cbOverlay.Checked = true;
             this.cbOverlay.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbOverlay.Location = new System.Drawing.Point(22, 270);
+            this.cbOverlay.Location = new System.Drawing.Point(16, 219);
+            this.cbOverlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbOverlay.Name = "cbOverlay";
-            this.cbOverlay.Size = new System.Drawing.Size(107, 21);
+            this.cbOverlay.Size = new System.Drawing.Size(84, 17);
             this.cbOverlay.TabIndex = 8;
             this.cbOverlay.Text = "Text overlay";
             this.cbOverlay.UseVisualStyleBackColor = true;
@@ -202,9 +215,10 @@
             // btnHelp
             // 
             this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHelp.Location = new System.Drawing.Point(598, 270);
+            this.btnHelp.Location = new System.Drawing.Point(448, 219);
+            this.btnHelp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(169, 74);
+            this.btnHelp.Size = new System.Drawing.Size(127, 60);
             this.btnHelp.TabIndex = 9;
             this.btnHelp.Text = "HELP";
             this.btnHelp.UseVisualStyleBackColor = true;
@@ -213,9 +227,10 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(773, 271);
+            this.btnSave.Location = new System.Drawing.Point(580, 220);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(169, 73);
+            this.btnSave.Size = new System.Drawing.Size(127, 59);
             this.btnSave.TabIndex = 10;
             this.btnSave.Text = "SAVE";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -227,9 +242,11 @@
             this.groupBox1.Controls.Add(this.btnUp);
             this.groupBox1.Controls.Add(this.btnRight);
             this.groupBox1.Controls.Add(this.btnLeft);
-            this.groupBox1.Location = new System.Drawing.Point(255, 270);
+            this.groupBox1.Location = new System.Drawing.Point(191, 219);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(162, 74);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(122, 60);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Adjust text overlay";
@@ -237,9 +254,10 @@
             // btnDown
             // 
             this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDown.Location = new System.Drawing.Point(57, 44);
+            this.btnDown.Location = new System.Drawing.Point(43, 36);
+            this.btnDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDown.Name = "btnDown";
-            this.btnDown.Size = new System.Drawing.Size(49, 24);
+            this.btnDown.Size = new System.Drawing.Size(37, 20);
             this.btnDown.TabIndex = 3;
             this.btnDown.Text = "\\/";
             this.btnDown.UseVisualStyleBackColor = true;
@@ -248,9 +266,10 @@
             // btnUp
             // 
             this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUp.Location = new System.Drawing.Point(57, 21);
+            this.btnUp.Location = new System.Drawing.Point(43, 17);
+            this.btnUp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUp.Name = "btnUp";
-            this.btnUp.Size = new System.Drawing.Size(49, 22);
+            this.btnUp.Size = new System.Drawing.Size(37, 18);
             this.btnUp.TabIndex = 2;
             this.btnUp.Text = "/\\";
             this.btnUp.UseVisualStyleBackColor = true;
@@ -259,9 +278,10 @@
             // btnRight
             // 
             this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRight.Location = new System.Drawing.Point(108, 21);
+            this.btnRight.Location = new System.Drawing.Point(81, 17);
+            this.btnRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRight.Name = "btnRight";
-            this.btnRight.Size = new System.Drawing.Size(49, 47);
+            this.btnRight.Size = new System.Drawing.Size(37, 38);
             this.btnRight.TabIndex = 1;
             this.btnRight.Text = ">";
             this.btnRight.UseVisualStyleBackColor = true;
@@ -270,9 +290,10 @@
             // btnLeft
             // 
             this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLeft.Location = new System.Drawing.Point(6, 21);
+            this.btnLeft.Location = new System.Drawing.Point(4, 17);
+            this.btnLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLeft.Name = "btnLeft";
-            this.btnLeft.Size = new System.Drawing.Size(49, 47);
+            this.btnLeft.Size = new System.Drawing.Size(37, 38);
             this.btnLeft.TabIndex = 0;
             this.btnLeft.Text = "<";
             this.btnLeft.UseVisualStyleBackColor = true;
@@ -281,17 +302,18 @@
             // lblCodeProgress
             // 
             this.lblCodeProgress.AutoSize = true;
-            this.lblCodeProgress.Location = new System.Drawing.Point(22, 298);
+            this.lblCodeProgress.Location = new System.Drawing.Point(16, 242);
+            this.lblCodeProgress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCodeProgress.Name = "lblCodeProgress";
-            this.lblCodeProgress.Size = new System.Drawing.Size(0, 17);
+            this.lblCodeProgress.Size = new System.Drawing.Size(0, 13);
             this.lblCodeProgress.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Honeydew;
-            this.ClientSize = new System.Drawing.Size(966, 353);
+            this.ClientSize = new System.Drawing.Size(724, 293);
             this.Controls.Add(this.lblCodeProgress);
             this.Controls.Add(this.pnlMain);
             this.Controls.Add(this.progressMain);
@@ -304,8 +326,9 @@
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(984, 400);
-            this.MinimumSize = new System.Drawing.Size(520, 400);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(742, 332);
+            this.MinimumSize = new System.Drawing.Size(394, 332);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PIN Utility v0.3.3";
